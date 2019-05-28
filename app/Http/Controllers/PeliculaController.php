35d958dd0b39bc36abff5805a5b2af87003e0bf5
wilxoncastillo <wilxoncastillo@gmail.com>
2019-05-28 19:55:53 -0400

    Prueba rutas 2

--- app/Http/Controllers/PeliculaController.php
+++ app/Http/Controllers/PeliculaController.php
@@ -5,6 +5,6 @@ namespace App\Http\Controllers;
 class PeliculaController extends Controller
 {
     public function genero(String $genero) {
-        //return $genero;
+        return $genero;
     }
 }
